96a9cf269e6ebc7f1683f95fc51dda47ab3e9c32
mradambeck <mr.adambeck@gmail.com>
2016-05-04 20:04:49 -0700

    changes routes to line by line

--- config/routes.rb
+++ config/routes.rb
@@ -1,27 +1,24 @@
 Rails.application.routes.draw do
-  root to: "cities#index"
 
-  resources :cities do
-    resources :posts
-  end
+  #|VERB   |URL                                |CONTROLLER             |ALIAS
+  root                                        to: "cities#index"
+
+  get     "/cities/:city_id/posts",           to: "posts#index",      as: "city_posts"
+  post    "/cities/:city_id/posts",           to: "posts#create"
+  get     "/cities/:city_id/posts/new",       to: "posts#new",        as: "new_city_post"
+  get     "/cities/:city_id/posts/:id/edit",  to: "posts#edit",       as: "edit_city_post"
+  get     "/cities/:city_id/posts/:id",       to: "posts#show",       as: "city_post"
+  patch   "/cities/:city_id/posts/:id",       to: "posts#update"
+  put     "/cities/:city_id/posts/:id",       to: "posts#update"
+  delete  "/cities/:city_id/posts/:id",       to: "posts#destroy"
+
+  get     "/cities",                          to: "cities#index",     as: "cities"
+  post    "/cities",                          to: "cities#create"
+  get     "/cities/new",                      to: "cities#new",       as: "new_city"
+  get     "/cities/:id/edit",                 to: "cities#edit",      as: "edit_city"
+  get     "/cities/:id",                      to: "cities#show",      as: "city"
+  patch   "/cities/:id",                      to: "cities#update"
+  put     "cities/:id",                       to: "cities#update"
+  delete  "cities/:id",                       to: "cities#destroy"
+
 end
-
-
-# Prefix          Verb   URI Pattern                               Controller#Action
-#   root          GET    /                                         cities#index
-# city_posts      GET    /cities/:city_id/posts(.:format)          posts#index
-#                 POST   /cities/:city_id/posts(.:format)          posts#create
-# new_city_post   GET    /cities/:city_id/posts/new(.:format)      posts#new
-# edit_city_post  GET    /cities/:city_id/posts/:id/edit(.:format) posts#edit
-# city_post       GET    /cities/:city_id/posts/:id(.:format)      posts#show
-#                 PATCH  /cities/:city_id/posts/:id(.:format)      posts#update
-#                 PUT    /cities/:city_id/posts/:id(.:format)      posts#update
-#                 DELETE /cities/:city_id/posts/:id(.:format)      posts#destroy
-# cities          GET    /cities(.:format)                         cities#index
-#                 POST   /cities(.:format)                         cities#create
-# new_city        GET    /cities/new(.:format)                     cities#new
-# edit_city       GET    /cities/:id/edit(.:format)                cities#edit
-#   city          GET    /cities/:id(.:format)                     cities#show
-#                 PATCH  /cities/:id(.:format)                     cities#update
-#                 PUT    /cities/:id(.:format)                     cities#update
-#                 DELETE /cities/:id(.:format)                     cities#destroy
