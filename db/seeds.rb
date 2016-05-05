19d5919b895d20258d4b25184f81cccdc0684b23
mradambeck <mr.adambeck@gmail.com>
2016-05-05 11:31:31 -0700

    creates a seed file for city and posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+City.destroy_all
+Post.destroy_all
+
+City.create({name: "San Francisco", state: "California", country: "United States"})
+Post.create({title: "Great Tacos", content:"Gotta love the tacos in SF. They're the best! Can't beat em. Nom nom nom.", city_id: 1})
+Post.create({title: "Worst Tacos", content:"UGH, I hate SF tacos! They are the worst. You should really go to somewhere more authentic, like Illinois, for a good taco. There they know how to make a good carne asada.", city_id: 1})
