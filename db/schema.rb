7f2321262514f45fa54019ab194ee9ef5c925cfb
Bkliger <bkliger@comcast.net>
2016-05-05 13:56:21 -0700

    revised user db and views

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160505165232) do
+ActiveRecord::Schema.define(version: 20160505200515) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20160505165232) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "current_city"
   end
 
   add_foreign_key "posts", "cities"
