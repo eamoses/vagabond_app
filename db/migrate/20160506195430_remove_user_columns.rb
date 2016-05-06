1600025b15bde32621985e0c946b57f4f486ed6d
eamoses <e.a.moses@gmail.com>
2016-05-06 13:12:25 -0700

    deleting one migrate from db

--- db/migrate/20160506195430_remove_user_columns.rb
+++ db/migrate/20160506195430_remove_user_columns.rb
@@ -4,7 +4,6 @@ remove_column(:users, :avatar_file_name)
 remove_column(:users, :avatar_content_type)
 remove_column(:users, :avatar_file_size)
 remove_column(:users, :avatar_updated_at)
-remove_column(:users, :slug)
 
   end
 end
