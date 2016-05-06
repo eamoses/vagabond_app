c3edebf0849828804f970cd069306b4c18e41df3
eamoses <e.a.moses@gmail.com>
2016-05-06 14:21:44 -0700

    truncated post text to limited number of characters

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160506195430) do
+ActiveRecord::Schema.define(version: 20160505211612) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
