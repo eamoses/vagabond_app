5473eddd2299c4300dad8cd393a31495fe032835
eamoses <e.a.moses@gmail.com>
2016-05-06 15:45:33 -0700

    migrated avatar info to database

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160505211612) do
+ActiveRecord::Schema.define(version: 20160506224322) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 20160505211612) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "current_city"
+    t.string   "avatar"
   end
 
   add_foreign_key "posts", "cities"
