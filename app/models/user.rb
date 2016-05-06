3abd13fef7e2a0c9c102c697ce14dd170062a13d
Bkliger <bkliger@comcast.net>
2016-05-06 12:57:19 -0700

    pretty urls and cleaned up user table

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,4 +21,8 @@ class User < ActiveRecord::Base
         @user = User.find_by({email: params[:email]})
         @user.try(:authenticate, params[:password])
     end
+
+    def to_param
+      "#{id} #{first_name} #{last_name}".parameterize
+    end
 end
