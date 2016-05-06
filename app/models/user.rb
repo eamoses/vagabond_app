766631aee32cd1ce0c1faeb5818f0f569f317d6c
eamoses <e.a.moses@gmail.com>
2016-05-06 12:54:48 -0700

    added regex to only include a-z for first and last name

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,15 @@ class User < ActiveRecord::Base
   has_many :posts
   has_secure_password
 
+VALID_NAME_REGEX = /[a-zA-Z]/
   validates :first_name,
-              presence:   true,
+              presence: true,
+              format:   { with: VALID_NAME_REGEX },
               length:   { maximum: 255 }
 
   validates :last_name,
-              presence:   true,
+              presence: true,
+              format:   { with: VALID_NAME_REGEX },
               length:   { maximum: 255 }
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
