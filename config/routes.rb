1e367e0f59ea7cb1e4505b9806e63576abe8dd19
eamoses <e.a.moses@gmail.com>
2016-05-07 13:55:35 -0700

    deleting changes made previously, they didnt work

--- config/routes.rb
+++ config/routes.rb
@@ -22,7 +22,7 @@ Rails.application.routes.draw do
   delete  "/cities/:id",                      to: "cities#destroy"
 
   get     "/users",                           to: "users#index",      as: "users"
-  post    "/users",                           to: "users#create",     as: "users"
+  post    "/users",                           to: "users#create"
   get     "/users/new",                       to: "users#new",        as: "new_user"
   get     "/users/:id/edit",                  to: "users#edit",       as: "edit_user"
   get     "/users/:id",                       to: "users#show",       as: "user"
